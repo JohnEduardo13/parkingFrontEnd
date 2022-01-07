65aaa161d250bcb94c629a062e41b78e9eaf7e2f
JohnEduardo13 <jhoneduardo13@gmail.com>
2022-01-07 16:52:11 -0500

    diseño del login

---
+++ lib/src/ui/welcome/welcomeScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:parking/src/ui/welcome/components/welcomeBody.dart';
+
+class WelcomeScreen extends StatelessWidget{
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return const Scaffold(
+      body: WelcomeBody(),
+    );
+  }
+}
